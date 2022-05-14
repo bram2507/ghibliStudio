8e678e9b8f79ea809f2886b862db30ef1e324161
bram2507 <abrahamalvarezgomez2507@gmail.com>
2022-05-14 17:30:39 +0200

    style: add svg ghibli logo and first page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,32 @@
 <template>
 	<div class="main_content">
-		<!-- <div class="main_content__title"></div> -->
-		<Card :film="film" v-for="(film, key) in films" :key="key" />
+		<div class="main_content__title">
+			<img src="@/assets/ghibli-logo.svg" alt="" />
+		</div>
+		<div>
+			<h1>Entrar</h1>
+		</div>
 	</div>
 </template>
 
 <script>
-import Card from "./Card.vue";
+// import Card from "./Card.vue";
 export default {
 	props: {},
-	components: {
-		Card,
-	},
+	// components: {
+	// 	Card,
+	// },
 	data() {
 		return {
+			show: false,
 			films: null,
 			characters: null,
 		};
 	},
 	created() {
+		setTimeout(() => {
+			this.show = !this.show;
+		}, 1000);
 		const characters = "https://ghibliapi.herokuapp.com/people";
 		const films = "https://ghibliapi.herokuapp.com/films";
 		const apiResp = this.getGhibliFilms(films);
@@ -60,19 +68,35 @@ export default {
 	padding: 0;
 	margin: 0;
 	box-sizing: border-box;
+	--curious-blue: #1c9ccc;
+	--gin: #e3eee9;
+	--tiber: #082a3e;
+	--blumine: #1c5e79;
+	--downy: #6fb7ce;
+	--jelly-bean: #2a7d9c;
+	--cello: #1d4c5a;
+	--seagull: #86d5e9;
+	--matisse: #176b94;
+	font-family: "Amaranth", sans-serif;
 }
 .main_content {
 	width: 100%;
 	min-height: 100vh;
-	background-color: #d4f1f4;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	background-color: var(--curious-blue);
 }
 
 .main_content__title {
-	margin-top: 5vh;
-	margin-top: 5vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.main_content__title img {
+	width: 55vw;
+	height: auto;
+	image-rendering: optimizeQuality;
 }
 </style>
